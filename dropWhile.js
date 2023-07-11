354f6625ee331954e3a211404dc05f946ef7e677
Elena Oblomova <lisinaeg@gmail.com>
2023-07-11 23:47:13 +0300

    feat: implement tests for dropWhile and filter

---
+++ dropWhile.js
@@ -0,0 +1,25 @@
+
+    // dropWhile _.dropWhile(array, [predicate=_.identity]) Creates a slice of array excluding elements dropped from the beginning. Elements are dropped until predicate returns falsey. The predicate is invoked with three arguments: (value, index, array).
+    /* var users = [
+  { 'user': 'barney',  'active': false },
+  { 'user': 'fred',    'active': false },
+  { 'user': 'pebbles', 'active': true }
+];
+
+_.dropWhile(users, function(o) { return !o.active; });
+// => objects for ['pebbles']
+
+// The `_.matches` iteratee shorthand.
+_.dropWhile(users, { 'user': 'barney', 'active': false });
+// => objects for ['fred', 'pebbles']
+
+// The `_.matchesProperty` iteratee shorthand.
+_.dropWhile(users, ['active', false]);
+// => objects for ['pebbles']
+
+// The `_.property` iteratee shorthand.
+_.dropWhile(users, 'active');
+// => objects for ['barney', 'fred', 'pebbles'] */
+export function dropWhile(array, predicate = item => item) {
+
+}
---
+++ test/filter.test.js
@@ -0,0 +1,95 @@
+import filter from '../filter';
+
+test('filter without parameters to equal empty array', () => {
+  expect(filter()).toStrictEqual([]);
+});
+
+test('filter empty object instead of array to equal empty array', () => {
+  expect(filter({})).toStrictEqual([]);
+});
+
+test('filter not empty object instead of array to equal empty array', () => {
+  expect(filter({a: 1})).toStrictEqual([1]);
+});
+
+test('filter NaN instead of array to equal empty array', () => {
+  expect(filter(NaN)).toStrictEqual([]);
+});
+
+test('filter number instead of array to equal empty array', () => {
+  expect(filter(3)).toStrictEqual([]);
+});
+
+test('filter array with -2 instead of predicate to equal empty array', () => {
+  const arr = ['a', 'b', 'c'];
+  expect(filter(arr, -2)).toEqual([]);
+  expect(arr).not.toEqual(filter(arr, -2));
+});
+
+test('filter empty array with default predicate to equal the same array', () => {
+  const arr = ['a', 'b', 'c'];
+  expect(filter(arr)).toStrictEqual(arr);
+  expect(filter(arr)).not.toBe(arr);
+});
+
+test('filter empty array with default predicate to equal empty array', () => {
+  expect(filter([])).toStrictEqual([]);
+});
+
+test('filter array with identity predicate to equal the same array', () => {
+  const arr = ['a', 'b', 'c'];
+  expect(filter(arr, item => item)).toStrictEqual(arr);
+  expect(filter(arr)).not.toBe(arr);
+});
+
+test('filter array with empty object as predicator to equal the same array', () => {
+  const arr = ['a', 'b', 'c'];
+  expect(filter(arr, {})).toStrictEqual(arr);
+  expect(filter(arr)).not.toBe(arr);
+});
+
+test('filter array with empty object as predicator to equal the same array', () => {
+  const arr = ['a', 'b', 'c'];
+  expect(filter(arr, {})).toStrictEqual(arr);
+  expect(filter(arr)).not.toBe(arr);
+});
+
+test('filter array with notActive function as predicator to array with matching items', () => {
+  var users = [
+    { 'user': 'barney', 'age': 36, 'active': true },
+    { 'user': 'fred',   'age': 40, 'active': false }
+  ];
+  exprct(filter(users, function (o) { return !o.active; }))
+    .toStrictEqual([{ 'user': 'fred', 'age': 40, 'active': false }]);
+  expect(filter(users, function (o) { return !o.active; })).not.toEqual(users);
+});
+
+test('filter array with matches predicator to equal array with matching items', () => {
+  var users = [
+    { 'user': 'barney', 'age': 36, 'active': true },
+    { 'user': 'fred',   'age': 40, 'active': false }
+  ];
+  exprct(filter(users, { 'age': 36, 'active': true }))
+    .toStrictEqual([{ 'user': 'barney', 'age': 36, 'active': true }]);
+  expect(filter(users, { 'age': 36, 'active': true })).not.toEqual(users);
+});
+
+test('filter array with matchesProperty predicator to equal array with matching items', () => {
+  var users = [
+    { 'user': 'barney', 'age': 36, 'active': true },
+    { 'user': 'fred',   'age': 40, 'active': false }
+  ];
+  exprct(filter(users, ['active', false]))
+    .toStrictEqual([{ 'user': 'fred', 'age': 40, 'active': false }]);
+  expect(filter(users, ['active', false])).not.toEqual(users);
+});
+
+test('filter array with property predicator to equal array with matching items', () => {
+  var users = [
+    { 'user': 'barney', 'age': 36, 'active': true },
+    { 'user': 'fred',   'age': 40, 'active': false }
+  ];
+  exprct(filter(users, 'active'))
+    .toStrictEqual([{ 'user': 'barney', 'age': 36, 'active': true }]);
+  expect(filter(users, 'active')).not.toEqual(users);
+});
