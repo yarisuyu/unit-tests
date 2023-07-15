cbc0c5e5314150b312b61003d87c1e1946782ce5
Elena Oblomova <lisinaeg@gmail.com>
2023-07-15 10:54:14 +0300

    feat: implement zip function

--- test/zip.test.js
+++ test/zip.test.js
@@ -1,45 +1,45 @@
-import zip from '../zip';
+import { zip } from '../zip';
 
 test('zip without parameters to equal empty array', () => {
-    expect(zip()).toBeStrictlyEqual([]);
+    expect(zip()).toStrictEqual([]);
 });
 
-test('zip from empty object to equal empty array', () => {
-    expect(zip({})).toBeStrictlyEqual([]);
+test('zip empty object to equal empty array', () => {
+    expect(zip({})).toStrictEqual([]);
 });
 
-test('zip from NaN instead of array to equal empty array', () => {
-    expect(zip(NaN)).toBeStrictlyEqual([]);
+test('zip NaN instead of array to equal empty array', () => {
+    expect(zip(NaN)).toStrictEqual([]);
 });
 
-test('zip from number instead of array to equal empty array', () => {
-    expect(zip(3)).toBeStrictlyEqual([]);
+test('zip number instead of array to equal empty array', () => {
+    expect(zip(3)).toStrictEqual([]);
 });
 
-test('zip from a not empty object with default predicate to equal the same array', () => {
-  expect(zip({a: 1})).toBeStrictlyEqual([]);
+test('zip a not empty object to equal the same array', () => {
+  expect(zip({a: 1})).toStrictEqual([]);
 });
 
-test('zip from empty array to equal empty array', () => {
-    expect(zip([])).toBeStrictlyEqual([]);
+test('zip empty array to equal empty array', () => {
+    expect(zip([])).toStrictEqual([]);
 });
 
-test('zip from array with -2 instead of predicate to equal the same array', () => {
-    expect(zip([1, 2, 3])).toBeStrictlyEqual([[1], [2], [3]]);
+test('zip array to equal array of arrays of single elements', () => {
+    expect(zip([1, 2, 3])).toStrictEqual([[1], [2], [3]]);
 });
 
-test('zip from array with -2 instead of predicate to equal the same array', () => {
-    expect(zip([1, 2, 3], [1, 2, 3])).toBeStrictlyEqual([[1, 1], [2, 2], [3, 3]]);
+test('zip array of 2 arrays to equal array of arrays of elements of 2 arrays groupped by index', () => {
+    expect(zip([1, 2, 3], [1, 2, 3])).toStrictEqual([[1, 1], [2, 2], [3, 3]]);
 });
 
-test('zip from array with -2 instead of predicate to equal the same array', () => {
-    expect(zip([1, 2, 3], ['a', 'b'])).toBeStrictlyEqual([[[1, "a"], [2, "b"], [3, undefined]]]);
+test('zip array of 2 arrays of different lengths to equal array of arrays of elements of 2 arrays or undefined groupped by index', () => {
+    expect(zip([1, 2, 3], ['a', 'b'])).toStrictEqual([[1, "a"], [2, "b"], [3, undefined]]);
 });
 
-test('zip from array with -2 instead of predicate to equal the same array', () => {
-    expect(zip([], ['a', 'b', 'c'])).toBeStrictlyEqual([[undefined, "a"], [undefined, "b"], [undefined, "c"]]);
+test('zip array of empty array and array of 3 elements to equal array of arrays of undefined and elements from array 2', () => {
+    expect(zip([], ['a', 'b', 'c'])).toStrictEqual([[undefined, "a"], [undefined, "b"], [undefined, "c"]]);
 });
 
-test('zip from array with -2 instead of predicate to equal the same array', () => {
-    expect(zip(['a', 'b'], [1, 2], [true, false])).toBeStrictlyEqual([['a', 1, true], ['b', 2, false]]);
+test('zip array of 3 arrays to equal array of arrays of elements of 3 arrays groupped by index', () => {
+    expect(zip(['a', 'b'], [1, 2], [true, false])).toStrictEqual([['a', 1, true], ['b', 2, false]]);
 });
--- zip.js
+++ zip.js
@@ -1,7 +1,30 @@
+// zip _.zip([arrays]) Creates an array of grouped elements, the first of which contains the first elements of the given arrays, the second of which contains the second elements of the given arrays, and so on.
+// _.zip(['a', 'b'], [1, 2], [true, false]);
+// => [['a', 1, true], ['b', 2, false]]
 
-  // zip _.zip([arrays]) Creates an array of grouped elements, the first of which contains the first elements of the given arrays, the second of which contains the second elements of the given arrays, and so on.
-  // _.zip(['a', 'b'], [1, 2], [true, false]);
-  // => [['a', 1, true], ['b', 2, false]]
-export function zip(arrays) {
+export function zip(...arrays) {
+  const DEFAULT_RESULT = [];
+  if (!Array.isArray(arrays)) {
+    return DEFAULT_RESULT;
+  }
 
+  let maxArrayLength = 0;
+  for (let i = 0; i < arrays.length; i += 1) {
+    if (arrays[i].length > maxArrayLength) {
+      maxArrayLength = arrays[i].length;
+    }
+  }
+
+  const result = [];
+  result.length = maxArrayLength;
+  for (let i = 0; i < maxArrayLength; i += 1) {
+    const zipped = [];
+    zipped.length = arrays.length;
+    for (let j = 0; j < arrays.length; j += 1) {
+      zipped[j] = arrays[j][i];
+    }
+    result[i] = zipped;
+  }
+
+  return result;
 }
