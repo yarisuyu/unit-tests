7519795a34de95b729c559a388cc7c9786ea9fbe
Elena Oblomova <lisinaeg@gmail.com>
2023-07-15 15:00:39 +0300

    feat: implement merge function

--- merge.js
+++ merge.js
@@ -1,7 +1,7 @@
-    // merge _.merge(object, [sources])
-    /*This method is like _.assign except that it recursively merges own and inherited enumerable string keyed properties of source objects into the destination object.
-    // Source properties that resolve to undefined are skipped if a destination value exists. Array and plain object properties are merged recursively.
-    // Other objects and value types are overridden by assignment. Source objects are applied from left to right. Subsequent sources overwrite property assignments of previous sources.
+// merge _.merge(object, [sources])
+/*This method is like _.assign except that it recursively merges own and inherited enumerable string keyed properties of source objects into the destination object.
+// Source properties that resolve to undefined are skipped if a destination value exists. Array and plain object properties are merged recursively.
+// Other objects and value types are overridden by assignment. Source objects are applied from left to right. Subsequent sources overwrite property assignments of previous sources.
 Note: This method mutates object.*/
 /* var object = {
   'a': [{ 'b': 2 }, { 'd': 4 }]
@@ -13,6 +13,55 @@ var other = {
 
 _.merge(object, other);
 // => { 'a': [{ 'b': 2, 'c': 3 }, { 'd': 4, 'e': 5 }] }*/
-export function merge(object, sources = []) {
 
+function mergeArray(array, source) {
+  const DEFAULT_RESULT = array;
+  if (typeof array !== 'object' || !Array.isArray(array)) {
+    return DEFAULT_RESULT;
+  }
+
+  const result = DEFAULT_RESULT;
+  for (let i = 0; i < result.length; i += 1) {
+    result[i] = mergeBase(result[i], source[i]);
+  }
+  return result;
+}
+
+function mergeBase(value, otherValue) {
+  if (typeof value === 'object') {
+    if (Array.isArray(value)) {
+      const result = mergeArray(value, otherValue);
+      return result;
+    } else {
+      const result = value;
+      const keys = Object.keys(otherValue);
+      for (let j = 0; j < keys.length; j += 1) {
+        result[keys[j]] = otherValue[keys[j]];
+      }
+      return result;
+    }
+  } else {
+    let result = value;
+    result = otherValue;
+
+    return result;
+  }
+}
+
+export function merge(obj, ...sources) {
+  const DEFAULT_RESULT = (typeof obj !== 'undefined') ? obj : {};
+  if (typeof obj !== 'object' || Array.isArray(obj)) {
+    return DEFAULT_RESULT;
+  }
+
+  const result = DEFAULT_RESULT;
+  for (let i = 0; i < sources.length; i += 1) {
+    const keys = Object.keys(sources[i]);
+    for (let j = 0; j < keys.length; j += 1) {
+      const merged = mergeBase(result[keys[j]], sources[i][keys[j]]);
+      result[keys[j]] = merged;
+    }
+  }
+
+  return result;
 }
--- test/merge.test.js
+++ test/merge.test.js
@@ -1,27 +1,27 @@
-import merge from '../merge';
+import { merge } from '../merge';
 
 test('merge without parameters to equal empty object', () => {
-  expect(merge()).toBeStrictlyEqual({});
+  expect(merge()).toStrictEqual({});
 });
 
 test('merge NaN instead of object to equal NaN', () => {
-  expect(merge(NaN)).toBeStrictlyEqual(NaN);
+  expect(merge(NaN)).toStrictEqual(NaN);
 });
 
 test('merge array instead of object to equal the same array', () => {
-  expect(merge([1, 2])).toBeStrictlyEqual([1, 2]);
+  expect(merge([1, 2])).toStrictEqual([1, 2]);
 });
 
 test('merge number instead of object to equal the same number', () => {
-  expect(merge(3)).toBeStrictlyEqual(3);
+  expect(merge(3)).toStrictEqual(3);
 });
 
 test('merge empty object to equal empty object', () => {
-  expect(merge({})).toBeStrictlyEqual({});
+  expect(merge({})).toStrictEqual({});
 });
 
 test('merge a not empty object to equal the same object', () => {
-  expect(merge({a: 1})).toBeStrictlyEqual({a: 1});
+  expect(merge({a: 1})).toStrictEqual({a: 1});
 });
 
 test('merge object to be the same object', () => {
@@ -37,28 +37,29 @@ test('merge 2 objects with the same property to equal object with the same prope
   var other = {
     'a': [{ 'c': 3 }, { 'e': 5 }]
   };
-  expect(merge(object, other)).toBeStrictlyEqual({ 'a': [{ 'b': 2, 'c': 3 }, { 'd': 4, 'e': 5 }] });
+  expect(merge(object, other)).toStrictEqual({ 'a': [{ 'b': 2, 'c': 3 }, { 'd': 4, 'e': 5 }] });
 });
 
 test('merge a not empty object with a number to equal the same object', () => {
-  expect(merge({a: 1}, 2)).toBeStrictlyEqual({a: 1});
+  expect(merge({a: 1}, 2)).toStrictEqual({a: 1});
 });
 
 test('merge a not empty object with the same object to equal the same object', () => {
-  expect(merge({a: 1}, {a: 1})).toBeStrictlyEqual({a: 1});
+  expect(merge({a: 1}, {a: 1})).toStrictEqual({a: 1});
 });
 
 test('merge 3 objects with the same properties to equal the object with merged property values', () => {
-  expect(merge({ a: 1 }, {a: 2, b: 3, c: 4}, {a: 3, b: 2})).toBeStrictlyEqual(
+  expect(merge({ a: 1 }, {a: 2, b: 3, c: 4}, {a: 3, b: 2})).toStrictEqual(
     {
-      a: 2,
-      b: 3
+      a: 3,
+      b: 2,
+      c: 4
     }
   );
 });
 
 test('merge a not empty object with array of objects to equal object with object added as a numeric property', () => {
-  expect(merge({ a: 1 }, [{ a: 2 }])).toBeStrictlyEqual(
+  expect(merge({ a: 1 }, [{ a: 2 }])).toStrictEqual(
     {
       0: {
         a: 2
@@ -69,13 +70,13 @@ test('merge a not empty object with array of objects to equal object with object
 });
 
 test('merge a not empty object with array of objects to equal object with objects added as numeric properties', () => {
-  expect(merge({a: 1}, [{a: 2}, {a: 3, c: 4}])).toBeStrictlyEqual(
+  expect(merge({a: 1}, [{a: 2}, {a: 3, c: 4}])).toStrictEqual(
     {
       0: {
         a: 2
       },
       1: {
-        b: 3,
+        a: 3,
         c: 4
       },
       a: 1
